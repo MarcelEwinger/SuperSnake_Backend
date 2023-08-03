1290f66e2dfa79f136ce62b5b8eb04ea1b1156c9
MarcelEwinger <marcel.ewinger@alumni.htl-klu.at>
2023-08-03 19:49:39 +0200

    d

--- server/game.js
+++ server/game.js
@@ -174,38 +174,33 @@ function randomFood(state) {
   state.food = food;//if the food is on an empty position
 }
 
-function getUpdatedVelocity(keyCode) {
+function getUpdatedVelocity(keyCode, velOld) {
   switch (keyCode) {
     case 37: { // left
-      return { x: -1, y: 0 };
+      if(velOld.x != 1){
+        return { x: -1, y: 0 };
+      }
+
+      
     }
     case 38: { // down
-      return { x: 0, y: -1 };
+      if(velOld.y != 1){
+        return { x: 0, y: -1 };
+      }
+      
     }
     case 39: { // right
-      return { x: 1, y: 0 };
+      if(velOld.x != -1){
+        return { x: 1, y: 0 };
+      }
+      
     }
     case 40: { // up
-      return { x: 0, y: 1 };
+      if(velOld.y != -1){
+        return { x: 0, y: 1 };
+      }
+      
     }
   }
 }
 
-////////////////////////////////////////////////////////////////
-
-function getUpdatedVelocityAndroid(keyCode) {
-  switch (keyCode) {
-    case 37: { // left
-      return { x: -1, y: 0 };
-    }
-    case 38: { // down
-      return { x: 0, y: -1 };
-    }
-    case 39: { // right
-      return { x: 1, y: 0 };
-    }
-    case 40: { // up
-      return { x: 0, y: 1 };
-    }
-  }
-}
